74971a2737b879d64ab55f09aaaaeb3449565f1f
Kashmellow <ejodlowsky@pm.me>
2020-02-25 22:43:10 -0500

    added BlogContext and initial seed data

--- masteryBlog/BlogContext.cs
+++ masteryBlog/BlogContext.cs
@@ -1,6 +1,40 @@
-﻿namespace masteryBlog
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using masteryBlog.Models;
+using System.Collections.Generic;
+
+
+namespace masteryBlog
 {
-    internal class BlogContext
+    public class BlogContext: DbContext
+
     {
+        public DbSet<BlogModel> Blog { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Blog;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString)
+                          .UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BlogModel>().HasData(
+                new BlogModel()
+                {
+                    BlogId = 1,
+                    Title = "First Blog",
+                    Content = "Here's some content about parenting",
+                    Author = "Svetlana",
+                    PublishDate = DateTime.Now,
+                    Category = "Parenting",
+                    Tags = "young parent life"
+               
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
