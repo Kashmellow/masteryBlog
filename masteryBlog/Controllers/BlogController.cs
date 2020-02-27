e5eb2ad5fca7b163cc4232663cf90c2e4f7fdca2
Kashmellow <ejodlowsky@pm.me>
2020-02-27 15:53:35 -0500

    added additional actions to BlogController, created BlogRepository

--- masteryBlog/Controllers/BlogController.cs
+++ masteryBlog/Controllers/BlogController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using masteryBlog.Models;
+using masteryBlog.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace masteryBlog.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        IRepository<BlogModel> blogRepo;
+
+        public BlogController(IRepository<BlogModel> blogRepo)
         {
-            return View();
+            this.blogRepo = blogRepo;
+        }
+        public ViewResult Index()
+        {
+            var model = blogRepo.GetAll();
+
+            return View(model);
+        }
+
+        public ViewResult Detail(int id)
+        {
+            var model = blogRepo.GetById(id);
+
+            return View(model);
         }
     }
 }
