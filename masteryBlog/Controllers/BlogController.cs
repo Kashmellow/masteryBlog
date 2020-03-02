a068c95a762ab0666b0bcd728f9509d66a2f1cfd
Kashmellow <ejodlowsky@pm.me>
2020-03-01 23:02:01 -0500

    Added Category Controller

--- masteryBlog/Controllers/BlogController.cs
+++ masteryBlog/Controllers/BlogController.cs
@@ -29,5 +29,60 @@ namespace masteryBlog.Controllers
 
             return View(model);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogModel blog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            blogRepo.Create(blog);
+            return RedirectToAction("Index");
+        }
+
+
+ 
+
+        [HttpGet]
+        public ViewResult Update(int id)
+        {
+            var model = blogRepo.GetById(id);
+            return View(model);
+        }
+
+        //Breaking Here
+        [HttpPost]
+        public ActionResult Update(BlogModel blog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            blogRepo.Update(blog);
+            return RedirectToAction("Detail", "Blog", new { id = blog.Id });
+        }
+
+
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            BlogModel model = blogRepo.GetById(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(BlogModel blog)
+        {
+            blogRepo.Delete(blog);
+            return RedirectToAction("Index", "Blog");
+        }
+
+
     }
 }
---
+++ masteryBlog/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace masteryBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
